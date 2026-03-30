399f122e7f963c815135ac6be141af4d9fe574ce
YiLy1 <sajolgayel1@gmail.com>
2026-03-30 08:50:48 +0800

    Initial Commit

---
+++ src/components/FabComponents.tsx
@@ -0,0 +1,16 @@
+import './FabComponents.css';
+
+interface ContainerProps { }
+
+const FabComponents: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Ready to create an app?</strong>
+      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
+  );
+};
+
+export default FabComponents;
+ 
+
