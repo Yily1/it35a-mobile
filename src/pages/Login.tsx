99bb3ca626e63477d69531dced8134235da13385
Yily1 <sajolgayel1@gmail.com>
2026-04-06 08:52:20 +0800

    Added Login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,34 @@
+import { IonButton, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+
+
+const Login: React.FC = () => {
+    const navigation = useIonRouter();
+
+    const doLogin = () => {
+        navigation.push('/app', 'forward', 'replace');
+    }
+    
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonMenuButton></IonMenuButton>
+          <IonTitle>Template</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+                    <IonContent fullscreen>
+                      <IonButton expand="full" onClick={() => doLogin()}>
+                        Login
+                      </IonButton>
+                    </IonContent>
+
+                     
+
+
+         </IonPage>
+
+  );
+
+};
+
+export default Login;
